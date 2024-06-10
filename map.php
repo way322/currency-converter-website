278463b8a97463cd73365386432affc69dc92784
way322 <iliyamalihin@yandex.ru>
2024-06-11 01:27:08 +0300

    Update map.php

--- map.php
+++ map.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <header>
         <div>
@@ -19,7 +21,7 @@
     </header>
     <div class="section2">
         <div class="container2">
-            
+            <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/213/moscow/search/%D0%9E%D0%B1%D0%BC%D0%B5%D0%BD%20%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Обмен валюты в Москве</a><a href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Москва</a><iframe src="https://yandex.ru/map-widget/v1/?display-text=%D0%9E%D0%B1%D0%BC%D0%B5%D0%BD%20%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B&ll=37.662760%2C55.751587&mode=search&sctx=ZAAAAAgBEAAaKAoSCZ5eKcsQz0JAEdOgaB7A4EtAEhIJldbfEoB%2F%2Fj8Rv30dOGfE5D8iBgABAgMEBSgKOABA1QFIAWoCcnWdAc3MTD2gAQCoAQC9AT%2F31sTCAY0B8fOfy7wD5%2Buw47UFiKj84ivawsXszAPpycrI3wXKi96C1gas4q%2BGlQb0rsmNnATCoZL1zwaUppCJ%2FwOu8aqCxwHg6ZSGsgOu6POGBrujsPkj2NWK9AO3%2F%2BWLBdrZuJiCBKTuoOvNA%2BbL07Ub9%2BPz4QX%2Fh%2BTsA5vHjoCWAv%2BIs%2FHHAeO3pPoEvoPNg8wCggIbKChjYXRlZ29yeV9pZDooMTg0MTA1NDA2KSkpigIJMTg0MTA1NDA2kgIAmgIMZGVza3RvcC1tYXBzqgItMjU3NTE4Mjc4OCwxMjYwNzY1MTI1LDI5MDM3NzA5MDksMjEzMDQzODAzMjk1&sll=37.662760%2C55.751587&sspn=0.414845%2C0.141256&text=%7B%22text%22%3A%22%D0%9E%D0%B1%D0%BC%D0%B5%D0%BD%20%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B%22%2C%22what%22%3A%5B%7B%22attr_name%22%3A%22category_id%22%2C%22attr_values%22%3A%5B%22184105406%22%5D%7D%5D%7D&z=12.2" width="560" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
         </div>
     </div>
     <header>
@@ -38,4 +40,5 @@
         </div>
     </header>
 </body>
+
 </html>
