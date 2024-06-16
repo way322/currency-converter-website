9d185e273a1f097dc60b21af6d914285043d6b8b
karasi001 <mishnefnara@gmail.com>
2024-06-17 00:49:44 +0300

    aaaaaa

--- index.php
+++ index.php
@@ -32,12 +32,12 @@
         <div class="create-line"></div>
     </div>
     <section class="section-Workspace section-convertor">
+    <h1 class="h1-convertor">Конвертер валют</h1>
         <div class="container-valit">
-            <h1 class="h1-convertor">Конвертер валют</h1>
             <div class="konverter1">
-                <h2>У меня есть</h2>
-                <input type="number" id="input" />
-                <select name="strani" id="input-currency">
+                <h2 class="h2-convertor">У меня есть</h2>
+                <input type="number" id="input" class="input-convertor convertor-input"/>
+                <select name="strani" id="input-currency" class="input-convertor convertor-select">
                     <option value="RUB">RUB — Рубли</option>
                     <option value="USD">USD — Доллар США</option>
                     <option value="EUR">EUR — Евро</option>
@@ -84,66 +84,67 @@
                     <option value="JPY">JPY - Японских иен</option>
                 </select>
             </div>
+        </div>
 
 
-            <button id="reverseButton"><img src="img/strelki.svg"></button>
+        <button id="reverseButton" class="button-revers"><img src="img/strelki.svg"></button>
 
 
-            <div class="container-valit">
-                <h2>Получаю</h2>
-                <input type="number" id="result" readonly />
-                <select name="strani" id="output-currency">
-                    <option value="RUB">RUB — Рубли</option>
-                    <option value="USD">USD — Доллар США</option>
-                    <option value="EUR">EUR — Евро</option>
-                    <option value="GBP">GBP — Фунт стерлингов</option>
-                    <option value="AUD">AUD - Австралийский доллар</option>
-                    <option value="AZN">AZN - Азербайджанский манат</option>
-                    <option value="AMD">AMD - Армянских драмов</option>
-                    <option value="BYN">BYN - Белорусский рубль</option>
-                    <option value="BGN">BGN - Болгарский лев</option>
-                    <option value="BRL">BRL - Бразильский реал</option>
-                    <option value="HUF">HUF - Венгерских форинтов</option>
-                    <option value="VND">VND - Вьетнамских донгов</option>
-                    <option value="HKD">HKD - Гонконгский доллар</option>
-                    <option value="GEL">GEL - Грузинский лари</option>
-                    <option value="DKK">DKK - Датская крона</option>
-                    <option value="AED">AED - Дирхам ОАЭ</option>
-                    <option value="EGP">EGP - Египетских фунтов</option>
-                    <option value="INR">INR - Индийских рупий</option>
-                    <option value="IDR">IDR - Индонезийских рупий</option>
-                    <option value="KZT">KZT - Казахстанских тенге</option>
-                    <option value="CAD">CAD - Канадский доллар</option>
-                    <option value="QAR">QAR - Катарский риал</option>
-                    <option value="KGS">KGS - Киргизских сомов</option>
-                    <option value="CNY">CNY - Китайский юань</option>
-                    <option value="MDL">MDL - Молдавских леев</option>
-                    <option value="NZD">NZD - Новозеландский доллар</option>
-                    <option value="NOK">NOK - Норвежских крон</option>
-                    <option value="PLN">PLN - Польский злотый</option>
-                    <option value="RON">RON - Румынский лей</option>
-                    <option value="XDR">XDR - СДР</option>
-                    <option value="SGD">SGD - Сингапурский доллар</option>
-                    <option value="TJS">TJS - Таджикских сомони</option>
-                    <option value="THB">THB - Таиландских батов</option>
-                    <option value="TRY">TRY - Турецких лир</option>
-                    <option value="TMT">TMT - Новый туркменский манат</option>
-                    <option value="UZS">UZS - Узбекских сумов</option>
-                    <option value="UAH">UAH - Украинских гривен</option>
-                    <option value="CZK">CZK - Чешских крон</option>
-                    <option value="SEK">SEK - Шведских крон</option>
-                    <option value="CHF">CHF - Швейцарский франк</option>
-                    <option value="RSD">RSD - Сербских динаров</option>
-                    <option value="ZAR">ZAR - Южноафриканских рэндов</option>
-                    <option value="KRW">KRW - Вон Республики Корея</option>
-                    <option value="JPY">JPY - Японских иен</option>
-                </select>
-            </div>
-            <div class="grafik">
-                <canvas id="dayChart" width="400" height="200"></canvas>
-                <canvas id="monthChart" width="400" height="200"></canvas>
-                <canvas id="yearChart" width="400" height="200"></canvas>
-            </div>
+        <div class="container-valit">
+            <h2 class="h2-convertor">Получаю</h2>
+            <input type="number" id="result" readonly class="input-convertor convertor-input"/>
+            <select name="strani" id="output-currency" class="input-convertor convertor-select">
+                <option value="RUB">RUB — Рубли</option>
+                <option value="USD">USD — Доллар США</option>
+                <option value="EUR">EUR — Евро</option>
+                <option value="GBP">GBP — Фунт стерлингов</option>
+                <option value="AUD">AUD - Австралийский доллар</option>
+                <option value="AZN">AZN - Азербайджанский манат</option>
+                <option value="AMD">AMD - Армянских драмов</option>
+                <option value="BYN">BYN - Белорусский рубль</option>
+                <option value="BGN">BGN - Болгарский лев</option>
+                <option value="BRL">BRL - Бразильский реал</option>
+                <option value="HUF">HUF - Венгерских форинтов</option>
+                <option value="VND">VND - Вьетнамских донгов</option>
+                <option value="HKD">HKD - Гонконгский доллар</option>
+                <option value="GEL">GEL - Грузинский лари</option>
+                <option value="DKK">DKK - Датская крона</option>
+                <option value="AED">AED - Дирхам ОАЭ</option>
+                <option value="EGP">EGP - Египетских фунтов</option>
+                <option value="INR">INR - Индийских рупий</option>
+                <option value="IDR">IDR - Индонезийских рупий</option>
+                <option value="KZT">KZT - Казахстанских тенге</option>
+                <option value="CAD">CAD - Канадский доллар</option>
+                <option value="QAR">QAR - Катарский риал</option>
+                <option value="KGS">KGS - Киргизских сомов</option>
+                <option value="CNY">CNY - Китайский юань</option>
+                <option value="MDL">MDL - Молдавских леев</option>
+                <option value="NZD">NZD - Новозеландский доллар</option>
+                <option value="NOK">NOK - Норвежских крон</option>
+                <option value="PLN">PLN - Польский злотый</option>
+                <option value="RON">RON - Румынский лей</option>
+                <option value="XDR">XDR - СДР</option>
+                <option value="SGD">SGD - Сингапурский доллар</option>
+                <option value="TJS">TJS - Таджикских сомони</option>
+                <option value="THB">THB - Таиландских батов</option>
+                <option value="TRY">TRY - Турецких лир</option>
+                <option value="TMT">TMT - Новый туркменский манат</option>
+                <option value="UZS">UZS - Узбекских сумов</option>
+                <option value="UAH">UAH - Украинских гривен</option>
+                <option value="CZK">CZK - Чешских крон</option>
+                <option value="SEK">SEK - Шведских крон</option>
+                <option value="CHF">CHF - Швейцарский франк</option>
+                <option value="RSD">RSD - Сербских динаров</option>
+                <option value="ZAR">ZAR - Южноафриканских рэндов</option>
+                <option value="KRW">KRW - Вон Республики Корея</option>
+                <option value="JPY">JPY - Японских иен</option>
+            </select>
+        </div>
+        <div class="grafik">
+            <canvas id="dayChart" width="400" height="200"></canvas>
+            <canvas id="monthChart" width="400" height="200"></canvas>
+            <canvas id="yearChart" width="400" height="200"></canvas>
+        </div>
     </section>
     <footer class="podval">
         <p>Мы используем файлы cookie для того, чтобы предоставить пользователям больше возможностей при посещении сайта
