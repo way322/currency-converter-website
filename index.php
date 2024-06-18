893a7723a02ba1ea5a0295f2c28fa6cddd67139b
karasi001 <mishnefnara@gmail.com>
2024-06-18 22:13:31 +0300

    aaaa

--- index.php
+++ index.php
@@ -151,15 +151,19 @@
         </div>
     </section>
     <footer class="podval contener-flex">
-        <p class="footer-text">Мы используем файлы cookie для того, чтобы предоставить пользователям больше возможностей при посещении сайта
-            Bastard.ru.</p>
-        <p class="footer-text">© 2009–2024, ООО «Бастард.ру». При использовании материалов гиперссылка на Bastard.ru обязательна. ИНН 7710718303,
-            ОГРН 1087746642774. 109544, г. Москва, Д.пушкина ул.Колотушкина д.12</p>
-        <div>
-            <h3>Ответим на ваши вопросы</h3>
-            <a href=""><img src="img/telega.svg"></a>
-            <a href=""><img src="img/mail.svg" alt=""></a>
-            <a href=""><img src="img/telefon.svg" alt=""></a>
+        <div class="text-div">
+            <p class="footer-text text-cookie">Мы используем файлы cookie для того, чтобы предоставить пользователям больше возможностей при посещении сайта
+                Bastard.ru.</p>
+            <p class="footer-text text-info">© 2009–2024, ООО «Бастард.ру». При использовании материалов гиперссылка на Bastard.ru обязательна. ИНН 7710718303,
+                ОГРН 1087746642774. 109544, г. Москва, Д.пушкина ул.Колотушкина д.12</p>
+        </div>
+        <div class="technical-support">
+            <h3 class="footer-otvet">Ответим на ваши вопросы</h3>
+            <ul>
+                <li><a href=""><img src="img/telega.svg"></a></li>
+                <li><a href=""><img src="img/mail.svg" alt=""></a></li>
+                <li><a href=""><img src="img/telefon.svg" alt=""></a></li>
+            </ul>
             <p>Служба заботы о клиентах
                 8 233 234 43 54
                 Ежедневно с 9:00 до 21:00 по МСК</p>
